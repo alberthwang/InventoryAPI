341b5d5b47dc62a32252028f351f0de44ab57679
alberthwang <albert.is.hwang@gmail.com>
2022-05-23 01:44:46 -0700

    added requirements, model updated
    added requirements file for venv, added quantity to items model,

--- InventoryAPI/InventoryApp/models.py
+++ InventoryAPI/InventoryApp/models.py
@@ -5,10 +5,12 @@ from django.db import models
 class Category(models.Model):
     CategoryId = models.AutoField(primary_key=True)
     CategoryName = models.CharField(max_length=500)
+    
 
 class Item(models.Model):
     ItemId = models.AutoField(primary_key=True)
     ItemName = models.CharField(max_length=500)
     Category = models.CharField(max_length=500)
+    Quantity = models.IntegerField()
     AmazonLink = models.CharField(max_length=500)
     InsertBy = models.CharField(max_length=20)
