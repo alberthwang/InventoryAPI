e1ae9af8a1d11d2f32dea5f3388c9a8febccfb1b
alberthwang <albert.is.hwang@gmail.com>
2022-05-24 03:09:20 -0700

    db migrations made, requirement updated
    updated requirements.txt to downgrade pymongo to 3.12.1 for djongo to run properly. db migrations made for models

--- InventoryAPI/InventoryApp/models.py
+++ InventoryAPI/InventoryApp/models.py
@@ -4,13 +4,22 @@ from django.db import models
 
 class Category(models.Model):
     CategoryId = models.AutoField(primary_key=True)
-    CategoryName = models.CharField(max_length=500)
+    CategoryName = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.name)
     
 
 class Item(models.Model):
     ItemId = models.AutoField(primary_key=True)
-    ItemName = models.CharField(max_length=500)
-    Category = models.CharField(max_length=500)
-    Quantity = models.IntegerField()
-    AmazonLink = models.CharField(max_length=500)
-    InsertBy = models.CharField(max_length=20)
+    ItemName = models.CharField(max_length=150)
+    Category = models.CharField(max_length=150)
+    Quantity = models.PositiveSmallIntegerField()
+    Details = models.TextField()
+    AmazonLink = models.CharField(max_length=150)
+    InsertBy = models.CharField(max_length=42)
+    InsertDate = models.DateField()
+    LastUpdated = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.name)
