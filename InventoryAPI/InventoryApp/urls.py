a56c35a874b60167fd37bdbccee0643fead5c74e
alberthwang <albert.is.hwang@gmail.com>
2022-06-30 13:50:19 -0700

    category base page
    category pages displays db info. env files moved to root

--- InventoryAPI/InventoryApp/urls.py
+++ InventoryAPI/InventoryApp/urls.py
@@ -7,10 +7,10 @@ from django.conf import settings
 
 
 urlpatterns=[
-    re_path(r'^category[\/]$', views.categoryApi),
+    re_path(r'^category$', views.categoryApi),
     re_path(r'^category/([0-9]+)$', views.categoryApi),
 
-    re_path(r'^item[\/]*$', views.itemApi),
+    re_path(r'^item$', views.itemApi),
     re_path(r'^item/([0-9]+)$', views.itemApi),
     
     re_path(r'^item/savefile', views.savefile)
--- ui/src/Category.js
+++ ui/src/Category.js
@@ -1,13 +1,50 @@
 import React,{Component} from 'react';
+import { Table } from 'react-bootstrap';
 
 export class Category extends Component{
 
-        render(){
-            return(
-                <div className="mt-5 d-flex justify-content-left">
-                    This is the Category page.
-                </div>
-            )
-        }
+    constructor(props){
+        super(props);
+        this.state={cats:[]}
+    }
+
+    refreshList(){
+        fetch(process.env.REACT_APP_API+'category')
+        .then(response=>response.json())
+        .then(data=>{this.setState({cats:data})});
+    }
+
+    componentDidMount(){
+        this.refreshList();
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+
+    render(){
+        const {cats}= this.state;
+        return(
+            <div >
+                <Table className='mt-4' striped bordered hover size='sm'>
+                    <thead>
+                        <tr>
+                            <th>CategoryId</th>
+                            <th>CategoryName</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cats.map(cat=>
+                            <tr key={cat.CategoryId}>
+                                <td>{cat.CategoryId}</td>
+                                <td>{cat.CategoryName}</td>
+                                <td>Edit/Delete</td>
+                            </tr>)}
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
     
 }
